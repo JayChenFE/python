87cb334cbb03d0564cd91aae1020ed820899b8a1
JayChenFE <chenjiehwy@163.com>
2017-10-15 09:57:39 +0800

    md ch09

--- fundamental/python_crash_course/exercise/ch09/9-7.py
+++ fundamental/python_crash_course/exercise/ch09/9-7.py
@@ -1,4 +1,4 @@
-#9-7
+# 9-7
 # 管理员 ：
 # 管理员是一种特殊的用户。
 # 编写一个名为Admin 的类，让它继承你为完成练习9-3或练习9-5而编写的User 类。
@@ -6,3 +6,24 @@
 # 用于存储一个由字符串（如"can add post" 、"can delete post" 、"can ban user" 等）组成的列表。
 # 编写一个名为show_privileges() 的方法，它显示管理员的权限。
 # 创建一个Admin 实例，并调用这个方法。
+class User:
+    def __init__(self, first_name, last_name, info):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.info = info
+
+    def describe_user(self):
+        print('{} {} is a {}'.format(self.first_name, self.last_name, self.info))
+
+
+class Admin(User):
+    def __init__(self, first_name, last_name, info, *privileges):
+        super().__init__(first_name, last_name, info)
+        self.privileges = list(privileges)
+
+    def show_privileges(self):
+        print(self.privileges)
+
+
+admin = Admin('mike', 'liu', 'dancer', 'can add post', 'can delete post', 'can ban user')
+admin.show_privileges()
--- fundamental/python_crash_course/exercise/ch09/9-8.py
+++ fundamental/python_crash_course/exercise/ch09/9-8.py
@@ -3,5 +3,32 @@
 # 编写一个名为Privileges 的类，
 # 它只有一个属性——privileges ，其中存储了练习9-7 所说的字符串列表。
 # 将方法show_privileges() 移到这个类中。
-# 在Admin 类中，# 将一个Privileges 实例用作其属性。
-# 创建一个Admin 实例，# 并使用方法show_privileges() 来显示其权限。
+# 在Admin 类中，将一个Privileges 实例用作其属性。
+# 创建一个Admin 实例，并使用方法show_privileges() 来显示其权限。
+
+class User:
+    def __init__(self, first_name, last_name, info):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.info = info
+
+    def describe_user(self):
+        print('{} {} is a {}'.format(self.first_name, self.last_name, self.info))
+
+
+class Privileges():
+    def __init__(self, privileges):
+        self.privileges = privileges
+
+    def show_privileges(self):
+        print(self.privileges)
+
+
+class Admin(User):
+    def __init__(self, first_name, last_name, info, *privileges):
+        super().__init__(first_name, last_name, info)
+        self.privileges = Privileges(list(privileges))
+
+
+admin = Admin('mike', 'liu', 'dancer', 'can add post', 'can delete post', 'can ban user')
+admin.privileges.show_privileges()
