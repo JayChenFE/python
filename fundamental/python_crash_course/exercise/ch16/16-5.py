12a89270164980d589a80dcdc94dc16bf74c797e
JayChenFE <chenjiehwy@163.com>
2017-10-26 22:15:25 +0800

    ch16

---
+++ fundamental/python_crash_course/exercise/ch16/16-5.py
@@ -0,0 +1,11 @@
+# 16-5
+# 涵盖所有国家 ：
+# 本节制作人口地图时，对于大约12个国家，程序不能自动确定其两个字母的国别码。
+# 请找出这些国家，在字典COUNTRIES 中找到它们的国别码；
+# 然后，对于每个这样的国家，都在get_country_code() 中添加一个if-elif 代码块，
+# 以返回其国别码：
+# if country_name == 'Yemen, Rep.'
+# return 'ye'
+# elif --snip--
+# 将这些代码放在遍历COUNTRIES 的循环和语句return None 之间。
+# 完成这样的修改后，你看到的地图将更完整。
